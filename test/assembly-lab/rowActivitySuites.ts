7902a39c9ce45f6a1142e64566a62a2a39935a6e
cgeng <cgeng@c3ng.com>
2026-09-17 10:17:24 +0800

    F-39：去掉未用到的类型 import（#138）

--- test/assembly-lab/rowActivitySuites.ts
+++ test/assembly-lab/rowActivitySuites.ts
@@ -37,7 +37,7 @@
  */
 import * as fsp from 'node:fs/promises'
 import * as path from 'node:path'
-import { openTreePage, withoutKnownNoise, type Check, type OpenedPage } from './harness.ts'
+import { openTreePage, withoutKnownNoise, type OpenedPage } from './harness.ts'
 import { LAB_TREES, type LabTreeRoute } from './labServer.ts'
 import { SCALE_TIERS } from '../../src/ui/assembly/shell/workspaceTree/styles.ts'
 import { listSessions } from '../../src/server/dshRpc.ts'
